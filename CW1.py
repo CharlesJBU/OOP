a43b0b02d50725b6947701d3b1644535d9fcab47
CharlesJBU <slonekerc@jbu.edu>
2025-08-27 09:53:21 -0500

    Add files via upload

---
+++ CW1.py
@@ -0,0 +1,6 @@
+print("Hello, wecome to OOP Class")
+
+name = input ("Enter your name:")
+age = input ("enter your age:")
+
+print = ("Welcome", name)
---
+++ Cw 2.py
@@ -0,0 +1,10 @@
+stu_name = input("enter the student name:")
+
+course1 = int(input("enter the grade points for course 1:"))
+course2 = int(input("enter the grade points for course 1:"))
+course3 = int(input("enter the grade points for course 1:"))
+course4 = int(input("enter the grade points for course 1:"))
+course5 = int(input("enter the grade points for course 1:"))
+
+total = course1+course2+course3+course4+course5
+print("The Total is:", total)

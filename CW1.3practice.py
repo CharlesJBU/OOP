32b06d18157e8ec6dccde203fba9670ae39b8ea7
CharlesJBU <slonekerc@jbu.edu>
2025-08-27 16:06:49 -0500

    Add files via upload

---
+++ CW1.3practice.py
@@ -0,0 +1,8 @@
+name = input("employee name:")
+
+wage = input("hourly wage:")
+hours = input("hours worked per day:")
+days = input("days worked in a week:")
+
+total_wages = wage+hours+days
+print("your total income is:", total_wages,"dollars per week")

1e82296e260f5d6dfc2133de9f4201c3e2a427a7
CharlesJBU <slonekerc@jbu.edu>
2025-10-08 09:47:26 -0500

    Add files via upload

--- CW 6.1.py
+++ CW 6.1.py
@@ -1,26 +1,48 @@
-Class Student:
-    def_init_(self):
+class Student:
+    def __init__(self):
         self.stu_id = ""
         self.stu_name = ""
         self.major = ""
-        self.gpa = ""
+        self.gpa = 0.0
         self.dob = ""
+
     def add_student(self):
         self.stu_id = input("Enter student ID:")
         self.stu_name = input("Enter student name:")
         self.stu_major = input("Enter student major:")
-        self.stu_gpa = int(input("Enter student gpa:"))
+        self.stu_gpa = float(input("Enter student gpa:"))
         self.stu_dob = input("Enter student date of birth:")
+        self.courses = []
+
     def edit_student(self):
         self.stu_id = input("Enter updated student ID:")
         self.stu_name = input("Enter updated student name:")
         self.stu_major = input("Enter updated student major:")
         self.stu_gpa = int(input("Enter updated student gpa:"))
         self.stu_dob = input("Enter updated student date of birth:")
+
+    def register_course(self, cc1):
+        self.courses.append(cc1)
+
     def display_student(self):
         print("stu_id:", self.stu_id)
-Course:
+        print("stu name:", self.stu_name)
+        for x in self.courses:
+            print("Course Registered:", self[x].courseName)
+class Course:
+    def __init__(self, cid, cname):
+        self.courseID = ""
+        self.courseName = ""
+    def add_course(self):
+        self.courseID = input("Input course name:")
+        self.courseName = input("Input course name:")
 
-Faculty:
+s1 = Student()
+s1.add_student()
+s1.display_student()
 
-Books:
+c1 = Course("CS1233", "OOP")
+
+
+
+s1.register_course(c1)
